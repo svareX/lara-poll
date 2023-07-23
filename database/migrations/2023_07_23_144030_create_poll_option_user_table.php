e3362103d64f8c351e62723826b34b61374d0eb0
svareX <stepanecek2003@gmail.com>
2023-07-23 16:53:23 +0200

    poll_id
    added column 'poll_id' into poll_option_user pivot table to ensure a user can only vote once for each poll

--- database/migrations/2023_07_23_144030_create_poll_option_user_table.php
+++ database/migrations/2023_07_23_144030_create_poll_option_user_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('poll_option_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('poll_id')->constrained()->cascadeOnDelete();
             $table->foreignId('poll_option_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
